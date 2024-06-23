e54ba7ae3257b1e02afb4eb6a5e5f429d26dca85
Davidkp8 <95929683+Davidkp8@users.noreply.github.com>
2024-06-23 23:42:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,116 +6,115 @@ import seaborn as sns
 import geopandas as gpd
 from shapely.geometry import Point
 
-# ----- Creador de mapas -----
+# ----- Map Creator -----
 
 def main():
-    # Configuración de la página
-    st.set_page_config(page_title="Alquiler Valencia")
+    # Page configuration
+    st.set_page_config(page_title="Valencia Rentals")
 
-    # Cargamos el fichero de datos y lo almacenamos en caché
+    # Load the data file and cache it
     @st.cache_data
     def load_data():
         return pd.read_csv(r"listings.csv")
 
-    # Pretratamiento del fichero de datos
+    # Data preprocessing
     df = load_data()
 
-    # Crear un widget de selección para las secciones
-    with st.sidebar:
-        st.header("Secciones")
-        pages = ("Distribución de Airbnb en Valencia", "Precios y tipos de habitación en Valencia")
-        selected_page = st.selectbox(
-            label="Elige la sección que deseas visualizar:",
-            options=pages)
+    # Create a selection widget for sections at the top
+    st.header("Valencia Rentals")
+    pages = ("Airbnb Distribution in Valencia", "Prices and Room Types in Valencia")
+    selected_page = st.selectbox(
+        label="Choose the section you want to view:",
+        options=pages)
 
-    ### ---- Distribución de Airbnb en Valencia ----
+    ### ---- Airbnb Distribution in Valencia ----
 
-    if selected_page == "Distribución de Airbnb en Valencia":
-        st.header("Distribución de los alquileres en Valencia")
-        st.subheader("Distribución de viviendas por barrios")
+    if selected_page == "Airbnb Distribution in Valencia":
+        st.header("Distribution of Rentals in Valencia")
+        st.subheader("Distribution of Houses by Neighborhoods")
         st.write(
-            "En este gráfico se representan las diferentes viviendas disponibles en Airbnb en Valencia. El color hace referencia al barrio en donde se sitúan.")
+            "This chart shows the different houses available on Airbnb in Valencia. The color indicates the neighborhood where they are located.")
 
-        # Mapa de las viviendas por barrios
+        # Map of houses by neighborhoods
         plt.figure(figsize=(10, 10))
 
-        # Crear el gráfico de dispersión usando seaborn
+        # Create the scatter plot using seaborn
         sns.scatterplot(x='longitude', y='latitude', hue='neighbourhood_group', s=20, data=df)
         st.set_option('deprecation.showPyplotGlobalUse', False)
 
-        # Añadir título y etiquetas de ejes
-        plt.xlabel('Longitud')
-        plt.ylabel('Latitud')
-        plt.title('Distribución de Airbnb en Valencia')
+        # Add title and axis labels
+        plt.xlabel('Longitude')
+        plt.ylabel('Latitude')
+        plt.title('Airbnb Distribution in Valencia')
 
-        # Añadir el mapa base de OpenStreetMap utilizando contextily
+        # Add the base map from OpenStreetMap using contextily
         ctx.add_basemap(plt.gca(), crs='EPSG:4326', source=ctx.providers.OpenStreetMap.Mapnik)
 
-        # Ajustar la leyenda para hacerla más discreta
-        plt.legend(title="Agrupaciones de Barrios", loc='lower left', fontsize='small')
+        # Adjust the legend to make it more discreet
+        plt.legend(title="Neighborhood Groups", loc='lower left', fontsize='small')
 
-        # Mostrar el gráfico en Streamlit
+        # Display the plot in Streamlit
         st.pyplot()
 
-        # Agregamos viviendas por barrio
-        st.subheader("Número de viviendas por barrio")
-        # Conteo de viviendas por barrio
+        # Add houses by neighborhood
+        st.subheader("Number of Houses by Neighborhood")
+        # Count houses by neighborhood
         neight_count = df.groupby('neighbourhood_group').size().reset_index(name='count')
-        cantidades = {elem[0]: elem[1] for elem in neight_count.values}
+        quantities = {elem[0]: elem[1] for elem in neight_count.values}
 
-        # Widget de selección para barrios
-        barrios = df['neighbourhood_group'].unique()
-        hood = st.selectbox('Selecciona un barrio:', barrios)
+        # Selection widget for neighborhoods
+        neighborhoods = df['neighbourhood_group'].unique()
+        hood = st.selectbox('Select a neighborhood:', neighborhoods)
 
-        # Mostrar el número de viviendas para el barrio seleccionado
-        if hood in cantidades:
-            st.write(f'El número de viviendas en {hood} es de {cantidades[hood]}')
+        # Display the number of houses for the selected neighborhood
+        if hood in quantities:
+            st.write(f'The number of houses in {hood} is {quantities[hood]}')
         else:
-            st.write(f'No hay datos disponibles para el barrio {hood}')
+            st.write(f'No data available for the neighborhood {hood}')
             
-        # Gráfico de barras del número de viviendas por barrio
-        st.subheader("Gráfico de número de viviendas por barrio")
+        # Bar chart of the number of houses by neighborhood
+        st.subheader("Bar Chart of Number of Houses by Neighborhood")
         plt.figure(figsize=(10, 5))
         sns.barplot(x='neighbourhood_group', y='count', data=neight_count)
-        plt.xlabel('Barrio')
-        plt.ylabel('Número de viviendas')
-        plt.title('Número de viviendas por barrio')
-        plt.xticks(rotation=45, ha='right')  # Rotar etiquetas del eje x
+        plt.xlabel('Neighborhood')
+        plt.ylabel('Number of Houses')
+        plt.title('Number of Houses by Neighborhood')
+        plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels
         st.pyplot()
 
-        # Mapa centrado en el barrio seleccionado
-        st.subheader("Mapa del barrio seleccionado")
-        # Crear GeoDataFrame de las viviendas
+        # Map centered on the selected neighborhood
+        st.subheader("Map of the Selected Neighborhood")
+        # Create GeoDataFrame of houses
         geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
         geo_df = gpd.GeoDataFrame(df, geometry=geometry)
         geo_df = geo_df.set_crs(epsg=4326)
 
-        # Filtrar datos para el barrio seleccionado
+        # Filter data for the selected neighborhood
         selected_geo_df = geo_df[geo_df['neighbourhood_group'] == hood]
 
-        # Crear el mapa
+        # Create the map
         fig, ax = plt.subplots(1, 1, figsize=(10, 10))
         base = selected_geo_df.plot(ax=ax, marker='o', color='red', markersize=5)
         ctx.add_basemap(ax, crs=geo_df.crs.to_string(), source=ctx.providers.OpenStreetMap.Mapnik)
-        plt.title(f'Mapa de viviendas en {hood}')
+        plt.title(f'Map of Houses in {hood}')
         st.pyplot(fig)
 
-    ### ---- Precios y tipos de habitación en Valencia ----
+    ### ---- Prices and Room Types in Valencia ----
 
-    if selected_page == "Precios y tipos de habitación en Valencia":
-        st.header("Análisis de los precios y tipos de habitación en Valencia")
-        st.subheader("Densidad y distribución de los precios por barrio")
+    if selected_page == "Prices and Room Types in Valencia":
+        st.header("Analysis of Prices and Room Types in Valencia")
+        st.subheader("Density and Distribution of Prices by Neighborhood")
     
-        # Lista de barrios de interés
+        # List of neighborhoods of interest
         nei_group_list = ['POBLATS MARITIMS', 'RASCANYA', 'EXTRAMURS', 'CAMPANAR', 'QUATRE CARRERES',
                           'CAMINS AL GRAU', 'LA SAIDIA', 'BENICALAP', 'JESUS', 'CIUTAT VELLA', 
                           "L'OLIVERETA", 'ALGIROS', 'EL PLA DEL REAL', "L'EIXAMPLE", 'PATRAIX', 
                           'BENIMACLET', 'POBLATS DEL SUD', "POBLATS DE L'OEST", 'POBLATS DEL NORD']
     
-        # Lista para almacenar los DataFrames de precios por grupo de vecindarios
+        # List to store the DataFrames of prices by neighborhood group
         price_list_by_n = []
     
-        # Obtener estadísticas sobre los rangos de precios para cada grupo de vecindarios
+        # Get statistics on the price ranges for each neighborhood group
         for group in nei_group_list:
             sub_df = df.loc[df['neighbourhood_group'] == group, 'price']
             stats = sub_df.describe(percentiles=[.25, .50, .75])
@@ -124,54 +123,55 @@ def main():
             stats.name = group
             price_list_by_n.append(stats)
     
-        # Concatenar todos los DataFrames en uno solo para mostrar la tabla final
+        # Concatenate all DataFrames into one to show the final table
         stat_df = pd.concat(price_list_by_n, axis=1)
     
-        # Mostrar la tabla con los precios mínimos, máximos y la media para cada barrio
+        # Display the table with the minimum, maximum, and average prices for each neighborhood
         st.write(
-            "Como podemos observar a continuación, los valores máximos de los precios para cada uno de los barrios son muy altos. Por tanto, vamos a establecer un límite de 500€ para poder realizar un mejor entendimiento y representación.")
+            "As we can see below, the maximum price values for each neighborhood are very high. Therefore, we will set a limit of 500€ to better understand and represent the data.")
         st.dataframe(stat_df)
     
-        # Creación del violinplot
+        # Creation of the violin plot
     
-        # Crear un sub-dataframe sin valores extremos / menores de 500
+        # Create a sub-dataframe without extreme values / less than 500
         sub_6 = df[df['price'] <= 500]
-        # Usar violinplot para mostrar la densidad y distribución de los precios
-        plt.figure(figsize=(12, 8))  # Ajusta el tamaño de la figura para mayor legibilidad
+        # Use violin plot to show the density and distribution of prices
+        plt.figure(figsize=(12, 8))  # Adjust the figure size for better readability
         viz_2 = sns.violinplot(data=sub_6, x='neighbourhood_group', y='price')
-        viz_2.set_title('Densidad y distribución de los precios para cada barrio')
-        viz_2.set_xlabel('Nombre del barrio')
-        viz_2.set_ylabel('Precio en €')
-        plt.xticks(rotation=45, ha='right')  # Rotar y alinear etiquetas del eje X
-        st.pyplot(plt.gcf())  # se utiliza plt.gcf() para obtener la figura actual
+        viz_2.set_title('Density and Distribution of Prices for Each Neighborhood')
+        viz_2.set_xlabel('Neighborhood Name')
+        viz_2.set_ylabel('Price in €')
+        plt.xticks(rotation=45, ha='right')  # Rotate and align x-axis labels
+        st.pyplot(plt.gcf())  # use plt.gcf() to get the current figure
         st.write(
-            "Con la tabla estadística y el gráfico de violín podemos observar algunas cosas sobre la distribución de precios de Airbnb en los distritos de Valencia. En primer lugar, podemos afirmar que algunos barrios tienen un rango de precios más alto para las publicaciones, con un precio promedio considerable. Esta distribución y densidad de precios pueden estar influenciadas por factores como la demanda turística y la oferta disponible.")
+            "With the statistical table and the violin plot, we can observe some things about the price distribution of Airbnb in Valencia districts. Firstly, we can affirm that some neighborhoods have a higher price range for listings, with a considerable average price. This price distribution and density can be influenced by factors such as tourist demand and available supply.")
     
-        # Tipo de habitación
+        # Room Type
 
-        st.subheader("Tipos de habitación por distrito")
-        hood1 = st.selectbox("Selecciona el barrio que deseas visualizar:", nei_group_list + ["Todos"])
-        agregado_price = sub_6.groupby(['neighbourhood_group', 'room_type']).agg({'price': 'mean'})
-        agregado_price1 = agregado_price
-        agregado_price1 = agregado_price1.reset_index()
-        if hood1 != "Todos":
+        st.subheader("Room Types by District")
+        hood1 = st.selectbox("Select the neighborhood you want to view:", nei_group_list + ["All"])
+        aggregated_price = sub_6.groupby(['neighbourhood_group', 'room_type']).agg({'price': 'mean'})
+        aggregated_price1 = aggregated_price
+        aggregated_price1 = aggregated_price1.reset_index()
+        if hood1 != "All":
             sub_7 = df.loc[df["neighbourhood_group"] == hood1]
             viz_3 = sns.catplot(x='neighbourhood_group', col='room_type', data=sub_7, kind='count')
             viz_3.set_xlabels('')
-            viz_3.set_ylabels('Nº de habitaciones')
+            viz_3.set_ylabels('Number of Rooms')
             viz_3.set_xticklabels(rotation=90)
             st.pyplot(viz_3)
-            st.write(f"Los precios promedios para cada tipo de habitación en el distrito {hood1} son:")
-            st.dataframe(agregado_price1.loc[agregado_price1["neighbourhood_group"] == hood1])
+            st.write(f"The average prices for each room type in the {hood1} district are:")
+            st.dataframe(aggregated_price1.loc[aggregated_price1["neighbourhood_group"] == hood1])
             st.write(
-                "Ten en cuenta que este promedio es teniendo en cuenta solo aquellos alquileres cuyo precio es inferior a 500 euros.")
+                "Note that this average takes into account only those rentals whose price is less than 500 euros.")
         else:
             st.pyplot(sns.catplot(x='neighbourhood_group', hue='neighbourhood_group', col='room_type', data=sub_6,
                                   kind="count"))
-            st.write("Estos son los precios promedio para cada habitación por barrio:")
-            st.dataframe(agregado_price)
+            st.write("These are the average prices for each room type by neighborhood:")
+            st.dataframe(aggregated_price)
             st.write(
-                "Ten en cuenta que este promedio es teniendo en cuenta solo aquellos alquileres cuyo precio es inferior a 500 euros.")
+                "Note that this average takes into account only those rentals whose price is less than 500 euros.")
 
 if __name__ == "__main__":
     main()
+
